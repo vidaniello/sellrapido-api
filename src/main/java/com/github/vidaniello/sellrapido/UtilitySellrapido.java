192c0a73fc677a10b54c661cad72a2c4dc3cbe92
Vincenzo D'Aniello <vidaniello@gmail.com>
2023-09-18 17:09:23 +0200

    first commit

---
+++ src/main/java/com/github/vidaniello/sellrapido/UtilitySellrapido.java
@@ -0,0 +1,10 @@
+package com.github.vidaniello.sellrapido;
+
+/**
+ * Utility for sellrapido API
+ * @author Vincenzo D'Aniello (vidaniello@gmail.com) github.com/vidaniello
+ *
+ */
+public class UtilitySellrapido {
+
+}
